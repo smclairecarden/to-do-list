58c8ee3f982158de2e460481883927118d71d44b
Claire Carden <smclairecarden@gmail.com>
2022-04-22 20:41:21 -0500

    edit functionality complete

--- controllers/todos.js
+++ controllers/todos.js
@@ -34,7 +34,29 @@ function show(req, res) {
 }
 
 function deleteToDo(req, res) {
-  console.log('delete!')
+  ToDo.findByIdAndDelete(req.params.id, function(err, todo) {
+    res.redirect('/todos')
+  })
+}
+
+function edit(req, res) {
+  ToDo.findById(req.params.id, function(err, todo) {
+    res.render('todos/edit', {
+      todo,
+      err, 
+      title: "Edit To Do"
+    })
+  })
+}
+
+function update(req, res) {
+  req.body.done = !!req.body.done
+  for (let key in req.body) {
+    if (req.body[key] === '') delete req.body[key]
+  }
+  ToDo.findByIdAndUpdate(req.params.id, req.body, function(err, todo) {
+    res.redirect(`/todos/${todo._id}`)
+  })
 }
 
 export {
@@ -43,4 +65,6 @@ export {
   index,
   show,
   deleteToDo as delete,
+  edit,
+  update,
 }
--- routes/todos.js
+++ routes/todos.js
@@ -5,8 +5,10 @@ import * as toDosCtrl from '../controllers/todos.js'
 router.get('/', toDosCtrl.index)
 router.get('/new', toDosCtrl.new)
 router.get('/:id', toDosCtrl.show)
+router.get('/:id/edit', toDosCtrl.edit)
 router.post('/', toDosCtrl
 .create)
+router.put('/:id', toDosCtrl.update)
 router.delete('/:id', toDosCtrl.delete)
 
 export {
